43480759d5bef689cfcf46b52724aeb8ad2f715c
Fakeroman <cristianroman_mancilla@ucol.mx>
2021-07-28 15:24:18 -0500

    login and more

--- controllers/auth.php
+++ controllers/auth.php
@@ -17,14 +17,51 @@ class Auth extends Controller{
     }
 
     function newRegister(){
-        $this->view->url = 'index/index';
-        $this->view->render('layout');
-
         $correo = $_POST['correo'];
         $nombre = $_POST['nombre'];
         $contra = $_POST['contra'];
+        
+        $validate = $this->model->searchEmail(['correo' => $correo]);
+         if ($validate == 1) {
+            $this->view->url = 'auth/register';
+            $this->view->render('layout');
+             echo '<script> alert("Ese correo ya existe") </script>';
+         } else {
+            $this->view->url = 'index/index';
+            $this->view->render('layout');
+            $this->model->insert(['correo' => $correo, 'nombre' => $nombre, 'contra' => $contra]);
+         }
+         
+    }
 
-        $this->model->insert(['correo' => $correo, 'nombre' => $nombre, 'contra' => $contra]);
+    function userLogin(){
+
+
+        $correo = $_POST['correo'];
+        $contra = $_POST['contra'];
+
+        $validate = $this->model->searchEmail(['correo' => $correo]);
+        if ($validate == 1) {
+
+            $login = $this->model->login(['correo' => $correo, 'contra' => $contra]);
+            if ($login) {
+                $this->view->url = 'index/index';
+                $this->view->render('layout');
+                echo '<script> alert("Inicio de sesion") </script>';
+            } else {
+                $this->view->url = 'auth/login';
+                $this->view->render('layout');
+                echo '<script> alert("El correo o la contraseña estan incorrectos") </script>';
+            }
+            
+
+         } else {
+            $this->view->url = 'auth/login';
+            $this->view->render('layout');
+            //  $this->model->insert(['correo' => $correo, 'nombre' => $nombre, 'contra' => $contra]);
+            echo '<script> alert("El correo o la contraseña estan incorrectos") </script>';
+         }
+
     }
 
 }
--- model/authModel.php
+++ model/authModel.php
@@ -12,6 +12,41 @@
              $query-> execute(['correo' => $data['correo'],'nombre' => $data['nombre'],'contra' => $data['contra']]);
             
         }
+        public function searchEmail($data){
+            $query = $this->db->connect()->prepare('SELECT correo FROM users WHERE correo = :correo');
+            if($query -> execute(['correo' => $data['correo']])) {
+                $out = [];
+                while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                    $out[] = $row["correo"];
+                }
+                if(empty($out)) {
+                    // Noo hay registros
+                   return  0;
+                } else {
+                    // si hay registros
+                   return 1;
+                }
+            } else {
+                return $db->errorInfo();
+            }
+        }
+        public function login($data){
+
+            $query = $this->db->connect()->prepare('SELECT correo FROM users WHERE correo = :correo AND contra = :contra');
+            if($query -> execute(['correo' => $data['correo'], 'contra' => $data['contra']])) {
+                $out = [];
+                $row = $query->fetch(PDO::FETCH_ASSOC);
+                if(empty($row)) {
+                    // Noo hay registros
+                   return  0;
+                } else {
+                    // si hay registros
+                   return 1;
+                }
+            } else {
+                return $db->errorInfo();
+            }
+        }
     }
 
 
